33de1de6f2f278dd4166b457e18ddc017024adca
noordseoostende <voropayev1@mail.ru>
2021-12-24 19:28:58 +0100

    notifications

---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import MainLayout from '../layouts/MainLayout';
+
+
+const Home = () => <MainLayout>Thuis</MainLayout>
+
+export default Home
---
+++ src/pages/Login.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import MainLayout from '../layouts/MainLayout';
+
+
+const Login = () => <MainLayout>Inloggen</MainLayout>
+
+export default Login
